dd4b6cf75feab818c8f41f47e035856c613f5cc9
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-11-01 00:28:52 +0200

    Week1, Exercise17, enums in sorting in top-method

--- viikko1/nhl-statistics-1/src/statistics.py
+++ viikko1/nhl-statistics-1/src/statistics.py
@@ -1,22 +1,33 @@
+from enum import Enum
 from player_reader import PlayerReader
+from player import Player
 
+class SortBy(Enum):
+    POINTS = 1
+    GOALS = 2
+    ASSISTS = 3
 
-def sort_by_points(player):
+def sort_by_points(player: Player):
     return player.points
 
+def sort_by_goals(player: Player):
+    return player.goals
+
+def sort_by_assists(player: Player):
+    return player.assists
 
 class Statistics:
     def __init__(self, reader: PlayerReader):
         self._players = reader.get_players()
 
-    def search(self, name):
+    def search(self, name: str):
         for player in self._players:
             if name in player.name:
                 return player
 
         return None
 
-    def team(self, team_name):
+    def team(self, team_name: str):
         players_of_team = filter(
             lambda player: player.team == team_name,
             self._players
@@ -24,12 +35,25 @@ class Statistics:
 
         return list(players_of_team)
 
-    def top(self, how_many):
-        sorted_players = sorted(
-            self._players,
-            reverse=True,
-            key=sort_by_points
-        )
+    def top(self, how_many: int, sort_by: SortBy = SortBy.POINTS):
+        if sort_by == SortBy.GOALS:
+            sorted_players = sorted(
+                self._players,
+                reverse=True,
+                key=sort_by_goals
+            )
+        elif sort_by == SortBy.ASSISTS:
+            sorted_players = sorted(
+                self._players,
+                reverse=True,
+                key=sort_by_assists
+            )
+        else:
+            sorted_players = sorted(
+                self._players,
+                reverse=True,
+                key=sort_by_points
+            )
 
         result = []
         i = 0
--- viikko1/nhl-statistics-1/src/tests/statistics_test.py
+++ viikko1/nhl-statistics-1/src/tests/statistics_test.py
@@ -1,14 +1,14 @@
 import unittest
-from statistics import Statistics
+from statistics import Statistics, SortBy
 from player import Player
 
 class PlayerReaderStub:
     def get_players(self):
         return [
             Player("Semenko", "EDM", 4, 12),
-            Player("Lemieux", "PIT", 45, 54), #99
+            Player("Lemieux", "PIT", 45, 54),
             Player("Kurri",   "EDM", 37, 53),
-            Player("Yzerman", "DET", 42, 56), #98
+            Player("Yzerman", "DET", 42, 56),
             Player("Gretzky", "EDM", 35, 89)
         ]
 
@@ -36,12 +36,42 @@ class TestStatistics(unittest.TestCase):
         str_list = list(map(lambda player: str(player), team))
         self.assertListEqual(str_list, correct_list)
 
-    def test_top_finds_correct_players(self):
+    def test_top_finds_correct_players_with_default_sorting_option(self):
         correct_list = [
             "Gretzky EDM 35 + 89 = 124",
             "Lemieux PIT 45 + 54 = 99",
             "Yzerman DET 42 + 56 = 98"
         ]
         top3 = self.statistics.top(3)
+        str_list = list(map(lambda player: str(player), top3))
+        self.assertListEqual(str_list, correct_list)
+
+    def test_top_finds_correct_players_by_points(self):
+        correct_list = [
+            "Gretzky EDM 35 + 89 = 124",
+            "Lemieux PIT 45 + 54 = 99",
+            "Yzerman DET 42 + 56 = 98"
+        ]
+        top3 = self.statistics.top(3, SortBy.POINTS)
+        str_list = list(map(lambda player: str(player), top3))
+        self.assertListEqual(str_list, correct_list)
+
+    def test_top_finds_correct_players_by_goals(self):
+        correct_list = [
+            "Lemieux PIT 45 + 54 = 99",
+            "Yzerman DET 42 + 56 = 98",
+            "Kurri EDM 37 + 53 = 90"
+        ]
+        top3 = self.statistics.top(3, SortBy.GOALS)
+        str_list = list(map(lambda player: str(player), top3))
+        self.assertListEqual(str_list, correct_list)
+
+    def test_top_finds_correct_players_by_assists(self):
+        correct_list = [
+            "Gretzky EDM 35 + 89 = 124",
+            "Yzerman DET 42 + 56 = 98",
+            "Lemieux PIT 45 + 54 = 99"
+        ]
+        top3 = self.statistics.top(3, SortBy.ASSISTS)
         str_list = list(map(lambda player: str(player), top3))
         self.assertListEqual(str_list, correct_list)
