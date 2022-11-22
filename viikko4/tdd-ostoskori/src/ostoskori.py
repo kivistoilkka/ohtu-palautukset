4dfba59ee8cb65bad06c2420d3d792e11aa362cc
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-11-22 16:59:52 +0200

    Week4 Exercise5, step15: Koodin siistiminen, kommentit pois

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -5,36 +5,26 @@ from ostos import Ostos
 class Ostoskori:
     def __init__(self):
         self._ostokset = {}
-        # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
         return reduce(lambda summa, ostos: summa+ostos.lukumaara(), self._ostokset.values(), 0)
-        # kertoo korissa olevien tavaroiden lukumäärän
-        # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
-        # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
 
     def hinta(self):
         return reduce(lambda summa, ostos: summa+ostos.hinta(), self._ostokset.values(), 0)
-        # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
-        # lisää tuotteen
         if lisattava.nimi() not in self._ostokset:
             self._ostokset[lisattava.nimi()] = Ostos(lisattava)
             return
         self._ostokset[lisattava.nimi()].muuta_lukumaaraa(1)
 
     def poista_tuote(self, poistettava: Tuote):
-        # poistaa tuotteen
         self._ostokset[poistettava.nimi()].muuta_lukumaaraa(-1)
         if self._ostokset[poistettava.nimi()].lukumaara() == 0:
             del self._ostokset[poistettava.nimi()]
 
     def tyhjenna(self):
         self._ostokset.clear()
-        # tyhjentää ostoskorin
 
     def ostokset(self):
         return list(self._ostokset.values())
-        # palauttaa listan jossa on korissa olevat ostos-oliot
-        # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
