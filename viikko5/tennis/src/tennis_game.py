55ed391b98726ebb232ecc8d37afc6296749e07d
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-12-02 17:47:35 +0200

    Week5 Exercise3, use names of players instead of hard coded names

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -6,9 +6,9 @@ class TennisGame:
         self.m_score2 = 0
 
     def won_point(self, player_name):
-        if player_name == "player1":
+        if player_name == self.player1_name:
             self.m_score1 = self.m_score1 + 1
-        else:
+        elif player_name == self.player2_name:
             self.m_score2 = self.m_score2 + 1
 
     def get_score(self):
@@ -30,13 +30,13 @@ class TennisGame:
             minus_result = self.m_score1 - self. m_score2
 
             if minus_result == 1:
-                score = "Advantage player1"
+                score = f"Advantage {self.player1_name}"
             elif minus_result == -1:
-                score = "Advantage player2"
+                score = f"Advantage {self.player2_name}"
             elif minus_result >= 2:
-                score = "Win for player1"
+                score = f"Win for {self.player1_name}"
             else:
-                score = "Win for player2"
+                score = f"Win for {self.player2_name}"
         else:
             for i in range(1, 3):
                 if i == 1:
