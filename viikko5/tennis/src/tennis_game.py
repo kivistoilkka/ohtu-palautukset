a881ed76cf2b1a315b6c04c134eedecf69c7119a
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-12-02 18:02:30 +0200

    Week5 Exercise3, players as dictionaries with name and score

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,49 +1,47 @@
 class TennisGame:
     def __init__(self, player1_name, player2_name):
-        self.player1_name = player1_name
-        self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.player1 = {'name': player1_name, 'score': 0}
+        self.player2 = {'name': player2_name, 'score': 0}
 
     def won_point(self, player_name):
-        if player_name == self.player1_name:
-            self.m_score1 = self.m_score1 + 1
-        elif player_name == self.player2_name:
-            self.m_score2 = self.m_score2 + 1
+        if player_name == self.player1['name']:
+            self.player1['score'] += 1
+        elif player_name == self.player2['name']:
+            self.player2['score'] += 1
 
     def get_score(self):
         score = ""
         temp_score = 0
 
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
+        if self.player1['score'] == self.player2['score']:
+            if self.player1['score'] == 0:
                 score = "Love-All"
-            elif self.m_score1 == 1:
+            elif self.player1['score'] == 1:
                 score = "Fifteen-All"
-            elif self.m_score1 == 2:
+            elif self.player1['score'] == 2:
                 score = "Thirty-All"
-            elif self.m_score1 == 3:
+            elif self.player1['score'] == 3:
                 score = "Forty-All"
             else:
                 score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
+        elif self.player1['score'] >= 4 or self.player2['score'] >= 4:
+            minus_result = self.player1['score'] - self.player2['score']
 
             if minus_result == 1:
-                score = f"Advantage {self.player1_name}"
+                score = f"Advantage {self.player1['name']}"
             elif minus_result == -1:
-                score = f"Advantage {self.player2_name}"
+                score = f"Advantage {self.player2['name']}"
             elif minus_result >= 2:
-                score = f"Win for {self.player1_name}"
+                score = f"Win for {self.player1['name']}"
             else:
-                score = f"Win for {self.player2_name}"
+                score = f"Win for {self.player2['name']}"
         else:
             for i in range(1, 3):
                 if i == 1:
-                    temp_score = self.m_score1
+                    temp_score = self.player1['score']
                 else:
                     score = score + "-"
-                    temp_score = self.m_score2
+                    temp_score = self.player2['score']
 
                 if temp_score == 0:
                     score = score + "Love"
