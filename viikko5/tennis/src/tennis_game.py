c91a2f978e6cda53ab0b59897e40d9a4920f6776
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-12-02 18:53:33 +0200

    Week5 Exercise3, score checking of first serves and in even matched in new methods, removed magic number

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,4 +1,6 @@
 class TennisGame:
+    UPPER_LIMIT_OF_FIRST_SERVES = 3
+
     def __init__(self, player1_name, player2_name):
         self.player1 = {'name': player1_name, 'score': 0}
         self.player2 = {'name': player2_name, 'score': 0}
@@ -19,6 +21,11 @@ class TennisGame:
         if score == 3:
             return "Forty"
 
+    def _check_even_match(self):
+        if self.player1['score'] <= TennisGame.UPPER_LIMIT_OF_FIRST_SERVES:
+            return self._score_name(self.player1['score'])+"-All"
+        return "Deuce"
+
     def _check_advantage_or_winner(self):
         difference = abs(self.player1['score'] - self.player2['score'])
         leader = max(self.player1, self.player2, key=lambda p: p['score'])
@@ -26,22 +33,17 @@ class TennisGame:
             return f"Win for {leader['name']}"
         return f"Advantage {leader['name']}"
 
+    def _check_match_in_first_serves(self):
+        score = self._score_name(self.player1['score'])
+        score += "-" + self._score_name(self.player2['score'])
+        return score
+
     def get_score(self):
         if self.player1['score'] == self.player2['score']:
-            if self.player1['score'] < 4:
-                return self._score_name(self.player1['score'])+"-All"
-            return "Deuce"
-
-        if max(self.player1['score'], self.player2['score']) >= 4:
+            return self._check_even_match()
+        if max(
+            self.player1['score'],
+            self.player2['score']
+        ) > TennisGame.UPPER_LIMIT_OF_FIRST_SERVES:
             return self._check_advantage_or_winner()
-
-        score = ""
-        temp_score = 0
-        for i in range(0, 2):
-            if i == 0:
-                temp_score = self.player1['score']
-            else:
-                score = score + "-"
-                temp_score = self.player2['score']
-            score += self._score_name(temp_score)
-        return score
+        return self._check_match_in_first_serves()
